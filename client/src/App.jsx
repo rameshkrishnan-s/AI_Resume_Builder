e9f7e4a72d544cdec0a7d260bb4a4438451d092b
ramesh <sundarrajanrameshkrishnan@gmail.com>
2025-10-11 20:42:39 +0530

    Initialized frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import Dashboard from "./pages/Dashboard";
+import Home  from "./pages/Home";
+import Layout  from "./pages/Layout";
+import { Route, Routes } from "react-router-dom";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
+
+const App = () =>{
+    return(
+        <>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="app" element={<Layout/>}>
+              <Route index element={<Dashboard/>}/>
+              <Route path="builder/:resumeId" element=
+              {<ResumeBuilder/>}/>
+            </Route>
+            <Route path="view/:resumeId" element={<Preview/>}/>
+            <Route path="login" element={<Login/>}/>
+          </Routes>
+        </>
+    )
+}
+
+export default App;
