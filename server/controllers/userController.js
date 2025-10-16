fa1caf85a6b1f6fd6fac9f151144fdae37546179
ramesh <sundarrajanrameshkrishnan@gmail.com>
2025-10-16 16:13:30 +0530

    backend complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,13 @@
-import User from "../models/User";
+import User from "../models/User.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import Resume from "../models/Resume.js";
+import protect  from "../middlewares/authMiddleware.js";
 
 
 const generateToken = (userId) =>{
-    const token = jwt.sign({userId})
+    const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn : '7d'})
+    return token;
 }   
 
 export const registerUser = async(req,res) => {
@@ -17,7 +20,7 @@ export const registerUser = async(req,res) => {
 
 
         const user = await User.findOne({email})
-        if(user){
+        if(user){   
             return res.status(400).json({message : 'User already exists'})
         }
 
@@ -26,8 +29,83 @@ export const registerUser = async(req,res) => {
 
         const newUser = await User.create({name,email,password : hashedPassword})
 
+        const token =  generateToken(newUser._id)
+        newUser.password = undefined;
+
+        return res.status(201).json({message : 'User created successfully', token,user : newUser})
+
 
     } catch (error) {
-        
+        return res.status(400).json({message : error.message})
+    }
+}
+
+
+//user login
+// /api/user/login
+
+
+
+export const loginUser = async(req,res) => {
+    try {
+        const {email,password} = req.body;
+
+
+        const user = await User.findOne({email})
+        if(!user){   
+            return res.status(400).json({message : 'Invalid user'})
+        }
+
+        if(!user.comparePassword(password)){
+            return res.status(400).json({message : 'Invalid'})
+        }
+
+
+        const token =  generateToken(user._id)
+        user.password = undefined;
+
+        return res.status(200).json({message : 'Login successfull', token,user : user})
+
+
+    } catch (error) {
+        return res.status(400).json({message : error.message})
+    }
+}
+
+// user by id
+//get /api/users/data
+
+
+export const getUserById = async(req,res) => {
+    try {
+        const userId = req.userId;
+        const user = await User.findById(userId)
+        if(!user){
+            return res.status(404).json({message : 'User not found'})
+        }
+
+
+        user.password = undefined;
+
+        return res.status(200).json({user})
+
+
+    } catch (error) {
+        return res.status(400).json({message : error.message})
+    }
+}
+
+//Controller for resume
+//get /api/users/resumes
+
+
+export const getUserResumes = async(req,res)=>{
+    try {
+        const userId = req.userId;
+
+        const resumes = await Resume.find({userId})
+        return res.status(200).json({resumes})
+    } catch (error) {
+        return res.status(400).json({message : error.message})
     }
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import userRouter from './routes/userRoutes.js';
+import resumeRouter from './routes/resumeRoutes.js';
+import aiRouter from './routes/aiRoutes.js';
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,6 +20,10 @@ app.get("/", (req, res) => {
   res.send("Server is live");
 });
 
+
+app.use('/api/users',userRouter)
+app.use('/api/resumes',resumeRouter)
+app.use('/api/ai/',aiRouter)
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
