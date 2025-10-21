3bd4de6d38cbc687f2014bc225dfaaa9ff4b7b4e
ramesh <sundarrajanrameshkrishnan@gmail.com>
2025-10-21 14:51:26 +0530

    removed button

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -82,7 +82,7 @@ const Hero = () => {
                                 ))}
                             </div>
                             <p className="text-sm text-gray-700">
-                                Used by 10,000+ users
+                                Used by 10+ users
                             </p>
                         </div>
                     </div>
@@ -100,10 +100,6 @@ const Hero = () => {
                             Get started
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right ml-1 size-4" aria-hidden="true"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg>
                         </Link>
-                        <button className="flex items-center gap-2 border border-slate-400 hover:bg-green-50 transition rounded-full px-7 h-12 text-slate-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-video size-5" aria-hidden="true"><path d="m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5"></path><rect x="2" y="6" width="14" height="12" rx="2"></rect></svg>
-                            <span>Try demo</span>
-                        </button>
                     </div>
 
                     <p className="py-6 text-slate-600 mt-14">Trusting by leading brands, including</p>
